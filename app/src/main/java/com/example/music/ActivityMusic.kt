125fb6f0eb92b1206bc1b1319b7f04b44443aff7
huyhz911 <huyngqe@gmail.com>
2022-06-02 17:30:45 +0700

    list item all song

--- app/src/main/java/com/example/music/ActivityMusic.kt
+++ app/src/main/java/com/example/music/ActivityMusic.kt
@@ -1,7 +1,11 @@
 package com.example.music
-
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import com.example.music.databinding.ActivityMainBinding
 
@@ -11,4 +15,6 @@ class ActivityMusic : AppCompatActivity() {
         super.onCreate(savedInstanceState)
      setContentView(R.layout.activity_main)
     }
+
+
 }
--- app/src/main/java/com/example/music/allSongs/AllSongsFragment.kt
+++ app/src/main/java/com/example/music/allSongs/AllSongsFragment.kt
@@ -1,15 +1,20 @@
 package com.example.music.allSongs
 
+import android.app.SearchManager
+import android.content.Context.SEARCH_SERVICE
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.view.MenuItemCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.example.music.databinding.AllSongsFragmentBinding
+import com.example.music.ActivityMusic
 import com.example.music.R
+import com.example.music.databinding.AllSongsFragmentBinding
 
-class AllSongsFragment:Fragment() {
+
+class AllSongsFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,4 +23,7 @@ class AllSongsFragment:Fragment() {
         val binding= DataBindingUtil.inflate<AllSongsFragmentBinding>(inflater,R.layout.all_songs_fragment,container,false)
         return  binding.root
     }
+
+
+
 }
